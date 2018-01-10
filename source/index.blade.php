6ddd12c4940b63996cac709dd5236b96da52c95f
Eduardo Gh <eduarguzher@gmail.com>
2018-01-09 19:50:45 -0500

    add new target on 4buyhub link

--- source/index.blade.php
+++ source/index.blade.php
@@ -47,7 +47,7 @@
 
                         <p class="my-1 font-semibold">Pregrado, Ingeniería Electrónica</p>
 
-                        <p><a class="hover:text-blue-darker" href="http://www.udea.edu.co">Universidad de Antioquia</a>
+                        <p><a class="hover:text-blue-darker" href="http://www.udea.edu.co" target="_blank">Universidad de Antioquia</a>
                         </p>
                     </li>
                 </ul>
@@ -78,7 +78,7 @@
         <h3 class="text-blue-darker uppercase text-sm mt-8 leading-loose tracking-wide font-semibold">Trabajo</h3>
         <div class="flex mt-5">
             <div class="w-1/2 pr-8">
-                <a class="hover:text-blue-dark" href="https://www.4buyhub.com/">
+                <a class="hover:text-blue-dark" href="https://www.4buyhub.com/" target="_blank">
                     <h4 class="text-3xl font-display">4BuyHub</h4>
                 </a>
                 <p class="my-4 leading-loose">
